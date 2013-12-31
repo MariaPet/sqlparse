91eba099e80cade1864cd6f3b48a29b18ac95c84
MariaPet <petriti.maria@gmail.com>
2013-12-31 20:30:28 +0200

    new method that tests the existance of a view in a query

--- sqlparse/experiments/view_handler.py
+++ sqlparse/experiments/view_handler.py
@@ -4,10 +4,9 @@ import sqlparse
 
 class ViewHandler(object):
     
-	def views_in_from(self,statement,given_views):
-		"""returns the views that are mentioned in the from clause of the query"""
-		parsed=sqlparse.parse(statement)#this a tuple of Statement objects
-		token_list = sql.TokenList(parsed[0].tokens)
+	def query_matched_views(self,token_list,given_views):
+		"""returns the views that exist in the from clause of the query
+		and also in the given views dictionary"""
 		for idx,token in enumerate(token_list.tokens):
 			if(isinstance(token,sql.IdentifierList)):
 				previous = token_list.token_prev(idx)
@@ -19,8 +18,24 @@ class ViewHandler(object):
 							if(subtoken.has_alias()):
 								view_alias = subtoken.get_alias()
 							if(view_name in given_views):
-								yield view_name.__str__(),view_alias.__str__()
-	
+								yield view_name.__str__(),view_alias
+								
+	def view_exist_in_query(self,token_list,view):
+		"""returns True if the specified view is found in the from clause of the query"""
+		exist = False
+		for idx,token in enumerate(token_list.tokens):
+			if(isinstance(token,sql.IdentifierList)):
+					previous = token_list.token_prev(idx)
+					if(previous.normalized == 'FROM' and previous.ttype==tokens.Token.Keyword):
+						for subtoken in token.get_sublists():
+							if(isinstance(subtoken,sql.Identifier)):
+								view_name = subtoken.get_real_name()
+								if(view_name == view):
+									exist = True
+		return exist
+								
+								
+			
 	def get_identifiers(self,token_list,identifiers_list=[])	:
 		""" recursive method that fetches identifiers from a token list"""						
 		for token in token_list.tokens:
@@ -42,20 +57,6 @@ class ViewHandler(object):
 		#parsed=sqlparse.parse(statement)#this a tuple of Statement objects
 		#token_list = sql.TokenList(parsed[0].tokens)
 		
-		#pairnei to alias tis view an exei, mporei na ginei ksexwristi methodos
-		for token in token_list.tokens:
-			if(isinstance(token,sql.IdentifierList)):
-				print 'to token einai I dentifierList : ',token
-				for identifier in token.get_sublists():
-					print 'sublist :',identifier
-					if(isinstance(identifier,sql.Identifier) and identifier.has_alias()):
-						if(identifier.get_real_name()==view):
-							view_alias = identifier.get_alias()
-							return view_alias
-					else:
-						print 'den exei alias'
-						return view 
-		#yield view
 		    	
 class GeneratedView(object):
 
@@ -84,12 +85,15 @@ class GeneratedView(object):
             
 test = ViewHandler()
 given_views = {'view1':('A1','A2','A3'),'view2':('A1','A2','A3','A4'),'view3':('A1','A2','A4')}
-sql_test = 'select v1.name as nom,V2.code,count(V2.id) from view1 as v1, view2 where v1.code like "12345" group by nom;'
-print dict(test.views_in_from(sql_test,given_views))
+sql_test = 'select v1.name as nom,V2.code,count(V2.id) from view1 as v1, view2,sales where v1.code like "12345" group by nom;'
+
 parsed = sqlparse.parse(sql_test)
 tokenlist = sql.TokenList(parsed[0].tokens)
+print dict(test.query_matched_views(tokenlist,given_views))
 identifiers = test.get_identifiers(tokenlist)
 for x,token in enumerate(identifiers):
 	print x, " ",token.normalized,' ',token.__class__.__name__
 	
 print test.get_view_attributes(tokenlist,'view1')
+
+print test.view_exist_in_query(tokenlist,'view1')

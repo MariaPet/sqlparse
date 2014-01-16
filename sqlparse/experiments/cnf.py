b85620c1336b735b64fe0f7ca66027bb5dbcf507
MariaPet <petriti.maria@gmail.com>
2014-01-16 16:02:37 +0200

    Fixing of regex captured matches & formation of the symbolic cnf string

--- sqlparse/experiments/cnf.py
+++ sqlparse/experiments/cnf.py
@@ -88,14 +88,14 @@ def sympylogic_to_symbolic(sympy_logic_str):
 	if(len(negations)>0):
 		for negative in negations:
 			symbols = re.findall(r'(?!Not)[A-Z]', negative)
-			print 'negatives stin arxiki formula: ',symbols
+			#print 'negatives stin arxiki formula: ',symbols
 			for symbol in symbols:
 				sympy_logic_str = sympy_logic_str.replace(negative, '~'+symbol);
 				
 	#If it is in simple cnf already return the clause
 	reg = re.compile('^And\((~?[A-Z],?\s?)*\)')
 	cnf = reg.match(sympy_logic_str)
-	print sympy_logic_str
+	#print sympy_logic_str
 	if(cnf is not None):
 		print 'cnf>0',cnf.group(),' klasi tis cnf ',cnf.__class__.__name__
 		sym = re.findall(r'(?!And)~?[A-Z]', cnf.group())
@@ -105,31 +105,36 @@ def sympylogic_to_symbolic(sympy_logic_str):
 			else:
 				symbolic_form += sy+' & '
 	
-	#if not in cnf
-	disjunctions =  re.findall(r'Or\(~?\w,?\s?~?\w\)', sympy_logic_str)
-	print disjunctions
+	#if not in cnf already convert disjunctions
+	disjunctions =  re.findall(r'Or\((?:~?\w,?\s?)*\)', sympy_logic_str)
 	if(len(disjunctions)>0):
-		isymbolic_form = ''
+		#symbolic_form = ''
 		for x,dis in enumerate(disjunctions):
+			symbolic_form = ''
 			symbolic_form += '('
 			symbols = re.findall(r'(?<!\w)~?\w(?!\w)',dis)
-			print symbols
 			for i,symbol in enumerate(symbols):
 				if(i == (len(symbols)-1)):
 					symbolic_form += symbol
 				else:
 					symbolic_form += symbol+' | '
 			symbolic_form += ')'
-			if(len(disjunctions)>1 and x!=len(disjunctions)-1):
-				symbolic_form += ' & '
-	print symbolic_form
+			sympy_logic_str = sympy_logic_str.replace(dis, symbolic_form)
+	
+	#replace the commas with ampersands and return symbolic cnf
+	commas = re.findall(r',',sympy_logic_str)
+	if(len(commas)>0):
+		for comma in commas:
+			sympy_logic_str = sympy_logic_str.replace(comma,' &')
+	sympy_logic_str = re.sub('And', '', sympy_logic_str)
+	print 'Teliki morfi se cnf : ',sympy_logic_str
 	
 
 	
 #--------------------------debugging-----------------------
 #sql_test = 'select v1.name as nom,V2.code,count(V2.id) from view1 as v1, view2 as V2 where (v1.code="12345" and v1.id<>2.9) or not( v1.id!=v2.id and v2.stuff=0) group by nom;'
-sql_test='select * from v1 where (v1.id!=123 or not v1.id=v2.name) and (v1.name="kati" or v1.code=g.kati);'
-#sql_test = 'select * from t where  v1=10 or not v2=5'
+#sql_test='select * from v1 where not(v1.id!=123 or  v1.id=v2.name) and (v1.name="kati" or v1.code=g.kati);'
+sql_test = 'select * from t where  v1=10 or not v2=5'
 parsed=sqlparse.parse(sql_test)
 for where in parsed[0].tokens:
 	if(isinstance(where,sql.Where)):
